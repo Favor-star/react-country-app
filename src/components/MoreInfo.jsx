37d76686b3618d1b170892c860d47caf586d94b0
favor-star <favoureliab@gmail.com>
2024-03-28 10:19:10 +0200

    setup gh-pages

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -47,12 +47,13 @@ const MoreInfo = ({ yes, names, yesClick }) => {
               aria-labelledby="offcanvasExampleLabel"
             >
               <div className="offcanvas-header border m-2 rounded border-3 border-opacity-50 border-primary bg-primary bg-opacity-25">
-                <h5
+                <h4
                   className="offcanvas-title border text-dark"
                   id="offcanvasExampleLabel"
                 >
                   {indexVal !== null && countries[indexVal].name.official}
-                </h5>
+                </h4>
+
                 <button
                   type="button"
                   className="btn-close"
@@ -61,16 +62,57 @@ const MoreInfo = ({ yes, names, yesClick }) => {
                 ></button>
               </div>
               <div className="offcanvas-body">
+                <div className="list-group bg-dark p-1 mb-4">
+                  <h6 className="text-center fw-bold text-white ">
+                    NATIVE NAMES
+                  </h6>
+                  o{indexVal !==null && Object.keys(countries[indexVal].name.nativeName).map(
+                    (name) => {
+                      return (
+                        <>
+                          <div className="list-group list-group-item mb-1 ">
+                            <span className="list-group-item bg-dark-subtle d-flex flex-column ">
+                              <span className="fw-bold bg-white border rounded border-primary text-primary text-center ">
+                                {name.toUpperCase()}
+                              </span>
+                              <span className="d-flex column-gap-1">
+                                <span className="fst-italic">Official:</span>
+                                <span className="fw-bold text-primary">
+                                  {
+                                    countries[indexVal].name.nativeName[name]
+                                      .official
+                                  }
+                                </span>
+                              </span>
+                              <span className="d-flex column-gap-1">
+                                <span className="fst-italic"> Common:</span>
+                                <span className="fw-bold text-primary">
+                                  {
+                                    countries[indexVal].name.nativeName[name]
+                                      .common
+                                  }
+                                </span>
+                              </span>{" "}
+                            </span>
+                          </div>
+                        </>
+                      );
+                    }
+                  )}
+                </div>
                 <div>
                   <div>
                     {indexVal !== null ? (
                       <>
-                        <div className="container  mx-auto ">
+                        <div className="container  mx-auto list-group bg-dark p-1 mb-4">
                           {indexVal !== checkIndex && <Spinner />}
                           {!isLoading && indexVal === checkIndex ? (
                             <>
+                              <h6 className=" fw-semibold  text-center text-white">
+                                COAT OF ARMS
+                              </h6>
                               <img
-                                className=" img-fluid d-flex flex-row mx-auto p-1 shadow"
+                                className=" img-fluid d-flex flex-row mx-auto p-1 shadow list-group-item rounded"
                                 style={{ maxHeight: "300px" }}
                                 src={
                                   Object.keys(countries[indexVal].coatOfArms)
@@ -80,28 +122,20 @@ const MoreInfo = ({ yes, names, yesClick }) => {
                                     ? countries[indexVal].coatOfArms.svg
                                     : ""
                                 }
-                                alt={`coatOfArms-of${countries[indexVal].name.official}`}
+                                alt={`Coat of arms of ${countries[indexVal].name.official}`}
                                 onLoad={handleImageLoad}
                               />
-                              <p className="fst-italic fw-semibold me-5 text-center">
-                                Coart Of Arms
-                              </p>
                             </>
                           ) : (
                             <Spinner />
                           )}
                         </div>
-                        <div>
-                          <span className="text-dark fw-semibold">
-                            Official Name:
-                          </span>
-                          {countries[indexVal].name.official}
-                        </div>
+
                         {console.log(countries[indexVal])}
                         <div className="list-group column-gap-1 bg-dark text-white  mb-3 p-1">
-                          <span className="fw-bold">
-                            Geographical Information:
-                          </span>
+                          <h6 className="fw-bold text-center">
+                            GEOGRAPHICAL INFORMATION
+                          </h6>
                           <div className=" d-flex  bg-dark-subtle p-2 rounded-top list-group-item">
                             <p className="fst-italic p-0 m-0 ">Region: </p>
                             <p className="fw-bold p-0 m-0 text-primary ">
@@ -132,21 +166,75 @@ const MoreInfo = ({ yes, names, yesClick }) => {
                               {countries[indexVal].latlng[0]}
                             </p>
                           </div>
-                          <div className="d-flex bg-dark-subtle column-gap-1  p-2 rounded-bottom list-group-item">
+                          <div className="d-flex bg-dark-subtle column-gap-1  p-2 list-group-item">
                             <p className="fst-italic p-0 m-0 ">Longitudes:</p>
                             <p className="fw-bold p-0 m-0 text-primary ">
                               {countries[indexVal].latlng[1]}
                             </p>
                           </div>
+                          <div className="d-flex justify-content-center bg-dark-subtle column-gap-1  p-2 rounded-bottom list-group-item">
+                            <div className="row">
+                              <div className="col-6 mx-auto">
+                                <div className="btn-group">
+                                  <button
+                                    className="btn btn-outline-primary dropdown-toggle"
+                                    type="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                  >
+                                    Time Zones
+                                  </button>
+                                  <ul className="dropdown-menu bg-secondary-subtle">
+                                    {countries[indexVal].timezones.map(
+                                      (val) => (
+                                        <li className="dropdown-item border-bottom border-1 border-white">
+                                          {val}
+                                        </li>
+                                      )
+                                    )}
+                                  </ul>
+                                </div>
+                              </div>
+                              {countries[indexVal].hasOwnProperty("borders") &&
+                              countries[indexVal].borders.length > 0 ? (
+                                <div className="col-6">
+                                  <div className="btn-group">
+                                    <button
+                                      className="btn btn-outline-primary  dropdown-toggle"
+                                      type="button"
+                                      data-bs-toggle="dropdown"
+                                      aria-expanded="false"
+                                    >
+                                      Borders
+                                    </button>
+                                    <ul className="dropdown-menu bg-secondary-subtle">
+                                      {countries[indexVal].borders.map(
+                                        (val) => (
+                                          <li className="dropdown-item border-bottom border-1 border-white">
+                                            {val}
+                                          </li>
+                                        )
+                                      )}
+                                    </ul>
+                                  </div>
+                                </div>
+                              ) : (
+                                <button className="col-5 btn btn-outline-primary btn-sm border border-primary rounded ">
+                                  No borders countries
+                                </button>
+                              )}
+                            </div>
+                          </div>
                         </div>
                         <div className="list-group bg-dark text-white  p-1">
-                          <span className="fw-bold">Currencies:</span>{" "}
-                          <span>
+                          <span className="fw-bold">Other Useful Info:</span>{" "}
+                          <span className="list-group-item p-1 my-1 rounded">
+                            <span>Currencies:</span>
                             {Object.keys(countries[indexVal].currencies).map(
                               (currency) => {
                                 return (
                                   <>
-                                    <div className="list-group-item bg-dark-subtle rounded-top">
+                                    <div className=" bg-dark-subtle rounded-top">
                                       <span className="fst-italic">Name: </span>
                                       <span className="fw-bold text-primary">
                                         {
@@ -156,7 +244,7 @@ const MoreInfo = ({ yes, names, yesClick }) => {
                                         }
                                       </span>
                                     </div>
-                                    <div className="list-group-item bg-dark-subtle rounded-bottom">
+                                    <div className=" bg-dark-subtle rounded-bottom">
                                       <span className="fst-italic">
                                         Symbol:{" "}
                                       </span>
@@ -173,55 +261,20 @@ const MoreInfo = ({ yes, names, yesClick }) => {
                               }
                             )}
                           </span>
+                          <span className="list-group-item list-group p-1 ">
+                            <span className="fst-italic">Languages:</span>
+                            {Object.keys(countries[indexVal].languages).map(
+                              (lang) => {
+                                return (
+                                  <span className="text-primary list-group-item bg-dark-subtle fw-bold ">
+                                    {countries[indexVal].languages[lang]}
+                                  </span>
+                                );
+                              }
+                            )}
+                          </span>
                         </div>
-                        <br />
-                        <div className="row">
-                          <div className="col-6">
-                            <div className="btn-group">
-                              <button
-                                className="btn btn-outline-primary dropdown-toggle"
-                                type="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                              >
-                                Time Zones
-                              </button>
-                              <ul className="dropdown-menu bg-secondary-subtle">
-                                {countries[indexVal].timezones.map((val) => (
-                                  <li className="dropdown-item border-bottom border-1 border-white">
-                                    {val}
-                                  </li>
-                                ))}
-                              </ul>
-                            </div>
-                          </div>
-                          {countries[indexVal].hasOwnProperty("borders") &&
-                          countries[indexVal].borders.length > 0 ? (
-                            <div className="col-6">
-                              <div className="btn-group">
-                                <button
-                                  className="btn btn-outline-primary  dropdown-toggle"
-                                  type="button"
-                                  data-bs-toggle="dropdown"
-                                  aria-expanded="false"
-                                >
-                                  Borders
-                                </button>
-                                <ul className="dropdown-menu bg-secondary-subtle">
-                                  {countries[indexVal].borders.map((val) => (
-                                    <li className="dropdown-item border-bottom border-1 border-white">
-                                      {val}
-                                    </li>
-                                  ))}
-                                </ul>
-                              </div>
-                            </div>
-                          ) : (
-                            <button className="col-5 btn btn-outline-primary btn-sm border border-primary rounded ">
-                              No borders countries
-                            </button>
-                          )}
-                        </div>
+                        
                       </>
                     ) : (
                       <Spinner />
